7771e6c34873557986b80a67b60bfa7adc98e6bb
mirelaf <mirela.ffr@gmail.com>
2023-04-21 15:21:48 +0200

    Link Addresses to Range

--- app/models/range.rb
+++ app/models/range.rb
@@ -1,3 +1,5 @@
 class Range < ApplicationRecord
   belongs_to :platform
+
+  has_many :addresses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_130707) do
+ActiveRecord::Schema.define(version: 2023_04_21_131542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2023_04_21_130707) do
   create_table "addresses", force: :cascade do |t|
     t.integer "status"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "range_id"
+    t.index ["range_id"], name: "index_addresses_on_range_id"
+  end
+
+  create_table "devices", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -34,6 +42,10 @@ ActiveRecord::Schema.define(version: 2023_04_21_130707) do
     t.integer "range_end"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "platform_id"
+    t.index ["platform_id"], name: "index_ranges_on_platform_id"
   end
 
+  add_foreign_key "addresses", "ranges"
+  add_foreign_key "ranges", "platforms"
 end
